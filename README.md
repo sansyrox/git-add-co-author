180947af021a5902b763830f61082fd22ae0fffb
Sanskar Jethi <sansyrox@gmail.com>
2023-04-28 14:19:24 -0600

    Update README

--- README.md
+++ README.md
@@ -1,27 +1,61 @@
-# Git Add Co Author
+# Git Add Co-Author
 
-## Description
+## Demo
 
-This is a simple script to add co-authors to your git commits. It will add the co-author to the commit message.
+
+## Overview
+
+Git Add Co-Author is a simple Python script that allows you to easily add co-authors to your Git commits, ensuring proper credit is given to all contributors involved in the development process. This can be particularly useful for open-source projects, where multiple contributors are working together, or for pair programming sessions where two developers contribute to the same commit.
 
 ## Installation
 
-1. Install the package using pip
+1. Install the package using pip:
+
 ```bash
 pip install git-add-co-author
 ```
 
-2. Obtain a token from GitHub
+2. Obtain a personal access token from GitHub. You can generate one by following the instructions in the [GitHub documentation](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token).
 
-3. Configure the token 
+3. Configure the script with your GitHub token:
 
 ```bash
-python -m git_add_co_author --token <token>
+python -m git_add_co_author --token <your_token>
 ```
 
 ## Usage
 
+To add a co-author to your commit, simply run the script with the co-author's GitHub username:
+
+```bash
+python -m git_add_co_author <co_author_username>
+```
+
+For example:
 
 ```bash
 python -m git_add_co_author sansyrox
 ```
+
+If you do not want to authorize a token, you can add the co-author's name and email address directly:
+
+```bash
+python -m git_add_co_author --name "John Doe" --email "john.doe@example.com"
+```
+
+### Optional Configuration
+
+I alias this command as `alias gac="python -m git_add_co_author"` in my `.zshrc` file, so I can simply run `gac <co_author_username>` to add a co-author to my commit.
+
+## Rationale
+
+The motivation behind creating this script is to provide an easy way to give credit to contributors in various situations:
+
+- In open-source projects, when a pull request (PR) is closed without merging but the idea or code is later implemented.
+- During pair programming sessions, where two or more developers work together on a single commit.
+
+Existing solutions were either too complicated or didn't work as expected, so Git Add Co-Author was developed as a simple, easy-to-use alternative.
+
+## License
+
+This project is licensed under the MIT License.
--- git_add_co_author/__main__.py
+++ git_add_co_author/__main__.py
@@ -1,34 +1,69 @@
 import requests
 import subprocess
 import click
+import shelve
+
+DB_PATH = "/tmp/add-co-author"
 
 
-@click.command()
-@click.argument('username')
-@click.option('--name', '-n', help='The name of the co-author.')
-@click.option('--email', '-e', help='The email address of the co-author.')
-@click.option('--token', '-t', help='Your GitHub personal access token.')
-def add_co_author(username, name='', email='', token=''):
+def add_co_author(username, name="", email="", token=""):
     if token:
-        subprocess.call(['git', 'config', '--global', 'github.token', token])
+        subprocess.call(["git", "config", "--global", "github.token", token])
     else:
-        token = subprocess.check_output(['git', 'config', '--global', 'github.token']).decode('utf-8').strip()
+        token = (
+            subprocess.check_output(["git", "config", "--global", "github.token"])
+            .decode("utf-8")
+            .strip()
+        )
     if not token:
-        raise ValueError('GitHub token not found. Please provide a token with --token or configure one with `git config --global github.token <token>`')
+        raise ValueError(
+            "GitHub token not found. Please provide a token with --token or configure one with `git config --global github.token <token>`"
+        )
 
     if not name and not email:
         headers = {"Authorization": f"Bearer {token}"}
-        response = requests.get(f'https://api.github.com/users/{username}', headers=headers)
+        response = requests.get(
+            f"https://api.github.com/users/{username}", headers=headers
+        )
         if response.status_code != 200:
-            raise ValueError(f'Error fetching GitHub profile for {username}: {response.status_code} {response.reason}')
+            raise ValueError(
+                f"Error fetching GitHub profile for {username}: {response.status_code} {response.reason}"
+            )
         data = response.json()
-        name = data.get('name', '')
-        email = data.get('email', '')
+        name = data.get("name", "")
+        email = data.get("email", "")
         if not name and not email:
-            raise ValueError(f'GitHub profile for {username} does not have a name or email address')
-    author = f'{name} <{email}>' if name and email else f'{name or email}'
-    subprocess.call(['git', 'commit', '--amend', '--trailer=' + f"Co-Authored-By: { author }"])
+            raise ValueError(
+                f"GitHub profile for {username} does not have a name or email address"
+            )
+    author = f"{name} <{email}>" if name and email else f"{name or email}"
+    subprocess.call(
+        ["git", "commit", "--amend", "--trailer=" + f"Co-Authored-By: { author }"]
+    )
 
-if __name__ == '__main__':
-    add_co_author()
 
+def save_token(token):
+    with shelve.open(DB_PATH) as db:
+        db["token"] = token
+
+
+def get_token():
+    with shelve.open(DB_PATH) as db:
+        return db.get("token", "")
+
+
+@click.command()
+@click.argument("username")
+@click.option("--name", "-n", help="The name of the co-author.")
+@click.option("--email", "-e", help="The email address of the co-author.")
+@click.option("--token", "-t", help="Your GitHub personal access token.")
+def main(username, name="", email="", token=""):
+    if token:
+        save_token(token)
+    else:
+        token = get_token()
+        add_co_author(username, name, email, token)
+
+
+if __name__ == "__main__":
+    main()
